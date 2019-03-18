e7750c69f50b3b091d2bd6be02d1622b4725ae5a
Ales Shenshin <shenshin@me.com>
2019-03-19 02:52:38 +0300

    Add pips draw for non-face cards

--- PlayingCard/View/PlayingCardView.swift
+++ PlayingCard/View/PlayingCardView.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class PlayingCardView: UIView {
 
-    var rank: Int = 13 { didSet { setNeedsDisplay(); setNeedsLayout() } }
-    var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var rank: Int = 10 { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var suit: String = "♣️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
     var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
 
     private var cornerString: NSAttributedString {
@@ -26,17 +26,6 @@ class PlayingCardView: UIView {
         label.numberOfLines = 0
         addSubview(label)
         return label
-    }
-
-    //draw нельзя вызывать явно. Он вызывается системой при отрисовке view
-    override func draw(_ rect: CGRect) {
-        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-        roundedRect.addClip()//это ограничивает дальнеейшее рисование границами данной фигуры
-        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).setFill()
-        roundedRect.fill()
-        if let faceCardImage = UIImage(named: rankString+suit) {
-            faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
-        }
     }
 
     //этот метод вызывается, когда view перерисовывается. Вызывать явно его не нужно! (как и draw)
@@ -86,6 +75,64 @@ class PlayingCardView: UIView {
         return NSAttributedString(string: string,
                                   attributes: [.paragraphStyle: paragraphStyle,
                                                .font: font])
+    }
+
+    //draw нельзя вызывать явно. Он вызывается системой при отрисовке view
+    override func draw(_ rect: CGRect) {
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        roundedRect.addClip()//это ограничивает дальнеейшее рисование границами данной фигуры
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).setFill()
+        roundedRect.fill()
+        if let faceCardImage = UIImage(named: rankString+suit) {
+            faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+        } else {
+            drawPips()
+        }
+    }
+
+    //отрисовка значков по центру карты для карт-цифр
+    func drawPips() {
+        //число значков в ряду для номера карты, кот. соотв. номеру элемента массива
+        let pipsPerRowForRank = [[0], [1], [1, 1], [1, 1, 1], [2, 2], [2, 1, 2],
+        [2, 2, 2], [2, 1, 2, 2], [2, 2, 2, 2], [2, 2, 1, 2, 2], [2, 2, 2, 2, 2]]
+
+        func createPipString(thatFits pipRect: CGRect) -> NSAttributedString {
+            let maxVerticalPipCount = CGFloat(pipsPerRowForRank.reduce(0) {max($1.count, $0)})
+            let maxHorizontalPipCount = CGFloat(pipsPerRowForRank.reduce(0) {max($1.max() ?? 0, $0)})
+            let verticalPipRowSpacing = pipRect.size.height / maxVerticalPipCount
+            let attemptedPipString = centeredAttributedString(suit, fontSize: verticalPipRowSpacing)
+            let probablyOkeyPipStringFontSize = verticalPipRowSpacing /
+                (attemptedPipString.size().height / verticalPipRowSpacing)//скобка не на том месте была
+            let probablyOkeyPipString = centeredAttributedString(suit, fontSize: probablyOkeyPipStringFontSize)
+            if probablyOkeyPipString.size().width > pipRect.size.width / maxHorizontalPipCount {
+                return centeredAttributedString(suit, fontSize: probablyOkeyPipStringFontSize /
+                (probablyOkeyPipString.size().width / (pipRect.size.width / maxHorizontalPipCount)))
+            } else {
+                return probablyOkeyPipString
+            }
+        }
+        //если номер такой карты-цифры существует в принципе,
+        if pipsPerRowForRank.indices.contains(rank) {
+            let pipsPerRow = pipsPerRowForRank[rank]
+            var pipRect = bounds.insetBy(dx: cornerOffset, dy: cornerOffset)
+                .insetBy(dx: cornerString.size().width, dy: cornerString.size().height / 2)
+            let pipString = createPipString(thatFits: pipRect)
+            let pipRowSpacing = pipRect.size.height / CGFloat(pipsPerRow.count)
+            pipRect.size.height = pipString.size().height
+            pipRect.origin.y += (pipRowSpacing - pipRect.size.height) / 2
+            for pipCount in pipsPerRow {
+                switch pipCount {
+                case 1:
+                    pipString.draw(in: pipRect)
+                case 2:
+                    pipString.draw(in: pipRect.leftHalf)
+                    pipString.draw(in: pipRect.rightHalf)
+                default:
+                    break
+                }
+                pipRect.origin.y += pipRowSpacing
+            }
+        }
     }
 }
 
